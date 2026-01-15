00a41750e0d24495fd8b5ec6d1e3afef74c25b3e
NewbieCoder212 <microsaasnb@proton.me>
2026-01-15 14:05:38 -0400

    FINAL 3 - pitch to business

--- src/app/+html.tsx
+++ src/app/+html.tsx
@@ -12,8 +12,11 @@ export default function Root({ children }: { children: React.ReactNode }) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-        {/* 
-          Disable body scrolling on web. This makes ScrollView components work closer to how they do on native. 
+        <title>Acadia Clean IQ</title>
+        <meta name="description" content="Washroom cleaning compliance tracking for NB Department of Health" />
+
+        {/*
+          Disable body scrolling on web. This makes ScrollView components work closer to how they do on native.
           However, body scrolling is often nice to have for mobile web. If you want to enable it, remove this line.
         */}
         <ScrollViewStyleReset />
